c7edc0970dc12d55079f4d7ee9e673d65327f4fe
Thomas Hsu <thomhsu@gmail.com>
2019-02-09 01:41:47 -0800

    Refactor to move image grid out of parent component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Slideshow from './components/Slideshow.jsx';
 
 class Carousel extends React.Component {
   constructor(props) {
@@ -21,42 +22,16 @@ class Carousel extends React.Component {
       )
   }
 
-  renderImg() {
+  renderImgGrid() {
     if (this.state.images) {
-      return(
-        <div className='img-grid container-fluid'>
-          <button type="button" className="share-btn btn btn-light"><i className="far fa-share-square"></i>   Share</button>
-          <button type="button" className="save-btn btn btn-light"><i className="far fa-heart"></i>   Save</button>
-          <button type="button" className="view-photo-btn btn btn-light">View Photos</button>
-          <div className='main row h-100'>
-            <div className='fill col'>
-              <img className='main-img image-responsive' src={this.state.images[0].imgUrl}/></div> 
-            <div className='col-sm-4 col-lg-3 border-0 d-none d-sm-block'>
-              <div className='fill row h-50 border-left-0 border-bottom-0'>
-                <img src={this.state.images[1].imgUrl}/>
-              </div>
-              <div className='fill row h-50 border-left-0'>
-                <img src={this.state.images[2].imgUrl}/>
-              </div>
-            </div>
-            <div className='col-lg-3 border-0 d-none d-lg-block'>
-              <div className='fill row h-50 border-left-0 border-bottom-0'>
-                <img src={this.state.images[3].imgUrl}/>
-              </div>
-              <div className='fill row h-50 border-left-0'>
-                <img src={this.state.images[4].imgUrl}/>
-              </div>
-            </div>
-          </div>
-        </div>
-      )
+      return (<Slideshow images={this.state.images}/>)
     }
   }
 
   render() {
     return (
       <div>
-        {this.renderImg()}
+        {this.renderImgGrid()}
       </div>
     )
   }
