cb58c633b9a801832a15836b6dc2b41271620ffb
Thomas Hsu <thomhsu@gmail.com>
2019-02-08 17:37:55 -0800

    Refactored out database fetchImages utility into own function

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const db = require('../db');
+const dbutils = require('../db/dbutils');
 
 let port = 3000;
 
@@ -10,16 +10,15 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/rooms/:listingId/images', cors(), (req, res) => {
   console.log('Heard a GET request');
-  db.from('images').select('*').where('listingId', '=', req.params.listingId).orderBy('imgOrder')
+  dbutils.fetchImages(req.params.listingId)
     .then(images => {
       console.log('Here are the images------------------------', images)
       res.send(JSON.stringify(images))
     })
     .then(() => console.log('...images sent! ------------------------'))
     .catch(err => {
-      console.log('Database retrieval failed------------');
+      console.log('Database retrieval failed------------------------', err);
     })
-
 })
 
 app.listen(port);
