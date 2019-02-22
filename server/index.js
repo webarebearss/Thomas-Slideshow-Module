1bc3006bba94b38190243a1d747204c883803156
Thomas Hsu <thomhsu@gmail.com>
2019-02-21 23:53:25 -0800

    Removed static folder to use S3 instead

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const app = express();
 const cors = require('cors');
 const dbutils = require('../db/dbutils');
 
-let port = 3001;
+const port = process.env.PORT || 3001;
 
-app.use('/rooms/:listingId/', express.static(__dirname + '/../client/dist'));
+// Include bundle.js in your index.html at https://s3.us-east-2.amazonaws.com/elasticbeanstalk-us-east-2-281924008100/bundle.js
 
 app.get('/rooms/:listingId/images', cors(), (req, res) => {
   console.log('Heard a GET request');
--- test/dom.test.js
+++ test/dom.test.js
@@ -2,27 +2,112 @@ import React from 'react';
 import { shallow, mount, render } from './enzyme';
 
 import PhotoModule from '../client/src/components/PhotoModule.jsx';
+import PhotoGrid from '../client/src/components/PhotoGrid.jsx';
 
-describe('<PhotoModule />', () => {
-  it('calls componentDidMount', () => {
-    sinon.spy(PhotoModule.prototype, 'componentDidMount');
-    const wrapper = mount(<PhotoModule />);
-    expect(PhotoModule.prototype.componentDidMount).to.have.property('callCount', 1);
+const samplePics = [ 
+  { imgId: 1,
+    imgUrl: 'http://lorempixel.com/640/480/city',
+    listingId: 1000,
+    imgOrder: 1,
+    description: 'Crest bluetooth Personal Loan Account' },
+  { imgId: 2,
+    imgUrl: 'http://lorempixel.com/640/480/nightlife',
+    listingId: 1000,
+    imgOrder: 2,
+    description: 'value-added Gloves auxiliary' },
+  { imgId: 3,
+    imgUrl: 'http://lorempixel.com/640/480/sports',
+    listingId: 1000,
+    imgOrder: 3,
+    description: 'Human' },
+  { imgId: 4,
+    imgUrl: 'http://lorempixel.com/640/480/cats',
+    listingId: 1000,
+    imgOrder: 4,
+    description: 'Forward' },
+  { imgId: 5,
+    imgUrl: 'http://lorempixel.com/640/480/sports',
+    listingId: 1000,
+    imgOrder: 5,
+    description: 'matrix' },
+  { imgId: 6,
+    imgUrl: 'http://lorempixel.com/640/480/fashion',
+    listingId: 1000,
+    imgOrder: 6,
+    description: 'Highway' },
+  { imgId: 7,
+    imgUrl: 'http://lorempixel.com/640/480/technics',
+    listingId: 1000,
+    imgOrder: 7,
+    description: 'Direct' },
+  { imgId: 8,
+    imgUrl: 'http://lorempixel.com/640/480/cats',
+    listingId: 1000,
+    imgOrder: 8,
+    description: 'HTTP Practical Shirt' },
+  { imgId: 9,
+    imgUrl: 'http://lorempixel.com/640/480/food',
+    listingId: 1000,
+    imgOrder: 9,
+    description: 'Gorgeous North Dakota backing up' },
+  { imgId: 10,
+    imgUrl: 'http://lorempixel.com/640/480/transport',
+    listingId: 1000,
+    imgOrder: 10,
+    description: 'Books Convertible Marks' },
+  { imgId: 11,
+    imgUrl: 'http://lorempixel.com/640/480/nature',
+    listingId: 1000,
+    imgOrder: 11,
+    description: 'alarm French Southern Territories Personal Loan Account' },
+  { imgId: 12,
+    imgUrl: 'http://lorempixel.com/640/480/abstract',
+    listingId: 1000,
+    imgOrder: 12,
+    description: 'Handcrafted Granite Shirt intranet Checking Account' },
+  { imgId: 13,
+    imgUrl: 'http://lorempixel.com/640/480/nightlife',
+    listingId: 1000,
+    imgOrder: 13,
+    description: 'Underpass Gorgeous Metal Chicken Bacon' },
+  { imgId: 14,
+    imgUrl: 'http://lorempixel.com/640/480/abstract',
+    listingId: 1000,
+    imgOrder: 14,
+    description: 'back-end De-engineered' },
+  { imgId: 15,
+    imgUrl: 'http://lorempixel.com/640/480/animals',
+    listingId: 1000,
+    imgOrder: 15,
+    description: 'scalable Product' },
+  { imgId: 16,
+    imgUrl: 'http://lorempixel.com/640/480/technics',
+    listingId: 1000,
+    imgOrder: 16,
+    description: 'Ukraine' },
+  { imgId: 17,
+    imgUrl: 'http://lorempixel.com/640/480/technics',
+    listingId: 1000,
+    imgOrder: 17,
+    description: 'Frozen program SDD' },
+  { imgId: 18,
+    imgUrl: 'http://lorempixel.com/640/480/transport',
+    listingId: 1000,
+    imgOrder: 18,
+    description: 'Mandatory generate exploit' } 
+];
+
+describe('<PhotoGrid />', () => {
+  const wrapper = shallow(<PhotoGrid images={samplePics} />);
+
+  it('renders images', () => {
+    expect(wrapper.find('#image1')).toBeDefined();
+    expect(wrapper.find('#image5')).toBeDefined();
   });
 
-  // it('allows us to set props', () => {
-  //   const wrapper = mount(<Foo bar="baz" />);
-  //   expect(wrapper.props().bar).to.equal('baz');
-  //   wrapper.setProps({ bar: 'foo' });
-  //   expect(wrapper.props().bar).to.equal('foo');
-  // });
+  it('renders modals', () => {
+    expect(wrapper.find('.share-modal')).toBeDefined();
+    expect(wrapper.find('.save-modal')).toBeDefined();
+  });
 
-  // it('simulates click events', () => {
-  //   const onButtonClick = sinon.spy();
-  //   const wrapper = mount((
-  //     <Foo onButtonClick={onButtonClick} />
-  //   ));
-  //   wrapper.find('button').simulate('click');
-  //   expect(onButtonClick).to.have.property('callCount', 1);
-  // });
 });
